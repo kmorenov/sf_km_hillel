ba19198504d9e60a3db9071c1fc16ded1ee7d959
Kostya Morenov <kostya.morenov@gmail.com>
2018-10-13 01:15:55 +0300

    AppFixtures.php

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -13,43 +13,34 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
 use App\Entity\Category;
-use App\Repository\CategoryRepository;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $min_cat = 0;
+        $max_cat = 5;
         //create 6 categories
         $categories = [];
-        for ($i = 0; $i < 6; $i++) {
+        for ($i = $min_cat; $i <= $max_cat; $i++) {
             $category = new Category();
             $category->setTitle('Category ' . $i);
             $category->setDescription('Category Description ' . $i);
 
-//            $categories[] = $category;
+            $categories[] = $category;
             $manager->persist($category);
-//            $manager->flush();
-
-            $article = new Article();
-            $article->setTitle('title ' . $i);
-            $article->setContent('content ' . $i);
-            $article->setCreatedAt(new \DateTime('now'));
-//            $category = $categories[0];
-            $article->setCategory($category->getId());
-            $manager->persist($article);
         }
         $manager->flush();
 
         // create 20 products
-        /*       for ($i = 0; $i < 20; $i++) {
-                  $article = new Article();
-                   $article->setTitle('title ' . $i);
-                   $article->setContent('content ' . $i);
-                   $article->setCreatedAt(new \DateTime('now'));
-                   $category = $categories[0];
-                   $article->setCategory($category->getId());
-                   $manager->persist($article);
+       for ($i = 0; $i < 20; $i++) {
+            $article = new Article();
+            $article->setTitle('title ' . $i);
+            $article->setContent('content ' . $i);
+            $article->setCreatedAt(new \DateTime('now'));
+            $article->setCategory($categories[mt_rand($min_cat, $max_cat)]);
+            $manager->persist($article);
         }
-        $manager->flush();*/
+        $manager->flush();
     }
 }
